68393a51f588720f23686fb162a383d1fd1a9961
Mahdiyar Sne <mahdiyarsne98@Gmail.com>
2025-04-06 12:30:12 +0330

    بخش سفارشی کردن تم اتمام شد

--- src/app/_services/theme.service.ts
+++ src/app/_services/theme.service.ts
@@ -1,9 +1,29 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Renderer2, RendererFactory2 } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ThemeService {
+  private render: Renderer2;
+  private currnetTheme: string = 'default';
 
-  constructor() { }
+  constructor(renderFactory: RendererFactory2) {
+    this.render = renderFactory.createRenderer(null, null);
+  }
+
+  setTheme(theme: string): void {
+    const htmlElement = document.documentElement;
+    this.render.setAttribute(htmlElement, 'data-theme', theme);
+    this.currnetTheme = theme;
+    localStorage.setItem('theme', theme);
+  }
+
+  loadTheme(): void {
+    const saveTheme = localStorage.getItem('theme') || 'default';
+    this.setTheme(saveTheme);
+  }
+
+  getTheme(): string {
+    return this.currnetTheme;
+  }
 }
