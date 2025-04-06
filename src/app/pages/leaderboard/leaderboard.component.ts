308717169c8352b883320b965d659a5eb2b3b165
Mahdiyar Sne <mahdiyarsne98@Gmail.com>
2025-04-06 14:28:27 +0330

    تابلوی امتیازات اتمام شد

--- src/app/pages/leaderboard/leaderboard.component.ts
+++ src/app/pages/leaderboard/leaderboard.component.ts
@@ -1,11 +1,68 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AfterViewInit, Component, inject, OnInit } from '@angular/core';
+import { LeaderboardEntry } from '../../_models/leaderboard-entry.model';
+import { LeaderboardService } from '../../_services/leaderboard.service';
 
 @Component({
   selector: 'app-leaderboard',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './leaderboard.component.html',
-  styleUrl: './leaderboard.component.scss'
+  styleUrl: './leaderboard.component.scss',
 })
-export class LeaderboardComponent {
+export class LeaderboardComponent implements OnInit, AfterViewInit {
+  showContent: boolean = false;
+  leaderboard: LeaderboardEntry[] = [];
+  currentPage: number = 1;
+  pageSize: number = 10;
+  totalRecords: number = 0;
+  totalPages: number = 0;
+  showLeaderboardMessage: boolean = false;
+  private leaderboardService = inject(LeaderboardService);
 
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.showContent = true;
+    }, 10);
+  }
+
+  loadLeaderboardCount(): void {
+    this.leaderboardService.getCount().subscribe({
+      next: (response) => {
+        (this.totalRecords = response.totalRecords),
+          (this.totalPages = Math.ceil(this.totalRecords / this.pageSize));
+        this.showLeaderboardMessage = true;
+      },
+      error: (err) => {
+        console.error('Error fatching leaderboard count: ', err);
+        this.showLeaderboardMessage = true;
+      },
+    });
+  }
+
+  loadPage(page: number) {
+    const start = (page - 1) * this.pageSize;
+    const limit = this.pageSize;
+    this.leaderboardService.getLeaderBoard(start, limit).subscribe((data) => {
+      this.leaderboard = data;
+    });
+  }
+
+  ngOnInit(): void {
+    this.loadLeaderboardCount();
+    this.loadPage(this.currentPage);
+  }
+
+  nextPage(): void {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+      this.loadPage(this.currentPage);
+    }
+  }
+
+  previousPage(): void {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.loadPage(this.currentPage);
+    }
+  }
 }
