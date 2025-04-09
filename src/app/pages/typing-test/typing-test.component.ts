c7b3be13bb260a29d9938b6edb13548b521c911c
Mahdiyar Sne <mahdiyarsne98@Gmail.com>
2025-04-09 15:01:53 +0330

    async

--- src/app/pages/typing-test/typing-test.component.ts
+++ src/app/pages/typing-test/typing-test.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { StatsComponent } from './stats/stats.component';
+import { InputRowComponent } from './input-row/input-row.component';
+import { WordDisplayComponent } from './word-display/word-display.component';
+import { GameService } from '../../_services/game/game.service';
 
 @Component({
   selector: 'app-typing-test',
-  imports: [],
+  imports: [
+    CommonModule,
+    StatsComponent,
+    InputRowComponent,
+    WordDisplayComponent,
+  ],
   templateUrl: './typing-test.component.html',
-  styleUrl: './typing-test.component.scss'
+  styleUrl: './typing-test.component.scss',
 })
-export class TypingTestComponent {
+export class TypingTestComponent implements OnInit, OnDestroy {
+  public gameService = inject(GameService);
 
+  ngOnInit(): void {
+    this.gameService.getWords().subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.gameService.restGame();
+    console.log('Game restart on component distuction');
+  }
 }
